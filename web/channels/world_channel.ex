9b61803d869e6a301ff5a0e67a141830dfa862cf
DanX <danx.exe@gmail.com>
2014-09-13 21:17:10 -0300

    Exclude sender from broadcast

--- web/channels/world_channel.ex
+++ web/channels/world_channel.ex
@@ -9,19 +9,19 @@ defmodule Game.WorldChannel do
     IO.puts "JOIN #{socket.channel}:#{socket.topic} player:#{player_id}"
 
     reply socket, "join", %{status: "connected"}
-    broadcast socket, "player:entered", %{player: player_id}
+    broadcast_from socket, "player:entered", %{player: player_id}
 
     {:ok, socket}
   end
 
   def leave(socket, _message) do
     player = socket |> Socket.get_assign :player
-    broadcast socket, "player:left", %{player: player}
+    broadcast_from socket, "player:left", %{player: player}
     socket
   end
 
   def event(socket, "sync", message) do
-    broadcast socket, "player:sync", message
+    broadcast_from socket, "player:sync", message
     socket
   end
 
